a319694487a56071e36534d70fbdc0227f3dd382
Chris <mr.chrisnash@gmail.com>
2021-09-30 16:56:21 -0700

    images

---
+++ src/images/index.js
@@ -0,0 +1,60 @@
+
+import bowser from '../images/MK8 Bowser.png';
+import dk from '../images/MK8 Donkey Kong.png';
+import daisy from '../images/MK8 Daisy.png';
+import iggy from '../images/MK8 Iggy.png';
+import inkling from '../images/MK8 Inkling Girl.png';
+import isabelle from '../images/MK8 Isabelle.png';
+import koopa from '../images/MK8 Koopa Troopa.png';
+import lakitu from '../images/MK8 Lakitu.png';
+import larry from '../images/MK8 Larry.png';
+import lemmy from '../images/MK8 Lemmy.png';
+import link from '../images/MK8 Link.png';
+import ludwig from '../images/MK8 Ludwig.png';
+import luigi from '../images/MK8 Luigi.png';
+import mario from '../images/MK8 Mario.png';
+import metal from '../images/MK8 Metal Mario.png';
+import mii from '../images/MK8 Mii.png';
+import morton from '../images/MK8 Morton.png';
+import peach from '../images/MK8 Peach.png';
+import pink from '../images/MK8 Pink Gold Peach.png';
+import rosalina from '../images/MK8 Rosalina.png';
+import roy from '../images/MK8 Roy.png';
+import shy from '../images/MK8 Shy Guy.png';
+import toad from '../images/MK8 Toad.png';
+import toadette from '../images/MK8 Toadette.png';
+import waluigi from '../images/MK8 Waluigi.png';
+import wario from '../images/MK8 Wario.png';
+import wendy from '../images/MK8 Wendy.png';
+import yoshi from '../images/MK8 Yoshi.png';
+
+export { 
+  bowser,
+  dk,
+  daisy,
+  iggy,
+  inkling,
+  isabelle,
+  koopa,
+  lakitu,
+  larry,
+  lemmy,
+  link,
+  ludwig,
+  luigi,
+  mario,
+  metal, 
+  mii,
+  morton,
+  peach,
+  pink,
+  rosalina,
+  roy,
+  shy,
+  toad,
+  toadette,
+  waluigi,
+  wario,
+  wendy,
+  yoshi
+};
