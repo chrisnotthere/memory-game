7069c7f47da0d931afc6cacbd3e81e35d56ddcf4
Chris <mr.chrisnash@gmail.com>
2021-10-01 11:51:01 -0700

    cards are now dynamically added

--- src/components/card_grid/Main.js
+++ src/components/card_grid/Main.js
@@ -1,56 +1,52 @@
 import React from "react";
 import '../../styles/Main.css'
 import Card from "react-bootstrap/Card";
-// import CardGroup from "react-bootstrap/CardGroup";
-//import Cards from "./Cards";
+import Cards from "./Cards";
+import { drawCards } from "../helpers/Draw";
 
 // eslint-disable-next-line no-unused-vars
 import { bowser,dk,iggy,koopa,lakitu,lemmy,ludwig,luigi,mario,mii,morton,roy,shy,toad,waluigi,wario,wendy,yoshi } from '../../images/index';
 
-// console.log(Cards);
 
 
 const Main = (props) => {
+  
+  let cardsToDraw = drawCards(props.imageCount);
+  console.log(`cards to draw: ${cardsToDraw}`);
+
+  //console.log({Cards});
+  let newCards = Cards.filter(card => cardsToDraw.includes(card.id));
+  console.log(newCards);
+
+
+  //let cardsToShow = Cards.filter(card => card.id === );
 
   return (
-    <>
-    {/* <p>images: {props.imageCount}</p> */}
-
     <main>
 
-      <Card.Img 
-        src={morton} 
+      {/* {props.Cards.map(card => (
+        // {console.log(card.name)}
+        <Card.Img 
+        key={card.id}
+        src={card.image} 
         style={{ width: '15rem', }}
         className='card'
         onClick={props.handleCardClick}
-      />
+        />
+      ))} */}
 
-      <Card.Img 
-        src={shy} 
+      {newCards.map(card => (
+        // {console.log(card.name)}
+        <Card.Img 
+        key={card.id}
+        src={card.image} 
         style={{ width: '15rem', }}
         className='card'
         onClick={props.handleCardClick}
-      />
-
-      <Card.Img 
-        src={mario} 
-        style={{ width: '15rem', }}
-        className='card'
-        onClick={props.handleCardClick}
-      />
-
-      <Card.Img 
-        src={luigi} 
-        style={{ width: '15rem', }}
-        className='card'
-        onClick={props.handleCardClick}
-      />
-            
-
-  
+        />
+      ))}
 
     </main>
-    </>
   )
 }
 
