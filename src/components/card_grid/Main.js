9e71817814a15da9f95249b6a08d1b7e5ea73b13
chris <mr.chrisnash@gmail.com>
2022-02-09 14:50:41 -0800

    comments and ui

--- src/components/card_grid/Main.js
+++ src/components/card_grid/Main.js
@@ -1,37 +1,34 @@
 import React from "react";
 import '../../styles/Main.css'
-// import Card from "react-bootstrap/Card";
 import Card from './Card'
 import Cards from "./Cards";
 import { drawCards } from "../helpers/Draw";
 
 const Main = (props) => {
-  
+
   const resetScore = () => {
     props.setCurrentScore(0);
     Cards.forEach(deckCard => deckCard.isClicked = false);
     console.log('resetting score and cards');
   }
-  
+
   const createNewCardArray = () => {
-    let cardsToDraw = drawCards(props.imageCount);  
+    let cardsToDraw = drawCards(props.imageCount);
     let newCards = Cards.filter(card => cardsToDraw.includes(card.id));
     return newCards;
   }
-  
+
   const handleCardClick = (e) => {
     let src = decodeURI(e.target.src.substring(21));
     let card = Cards.filter(deckCard => deckCard.image === src);
-    //console.log(card[0].name, card[0].isClicked);
-    
-    if(!card[0].isClicked) {
+
+    if (!card[0].isClicked) {
       props.setCurrentScore(props.currentScore + 1);
-      card[0].isClicked = true; 
-      //console.log('this card has not been clicked before');
+      card[0].isClicked = true;
     } else {
       alert(`whoops! You already clicked ${card[0].name}!`);
       resetScore();
-    }  
+    }
   }
 
   let newCards = createNewCardArray();
@@ -39,9 +36,9 @@ const Main = (props) => {
   return (
     <main>
       {newCards.map(card => (
-        <Card 
+        <Card
           key={card.id}
-          src={card.image} 
+          src={card.image}
           handleCardClick={handleCardClick}
         />
       ))}
